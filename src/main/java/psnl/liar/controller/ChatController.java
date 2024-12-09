764cb6df62caa3ce9b0e5bf412968eeb40202461
Philips413 <philips413@kakao.com>
2024-12-09 12:59:35 +0900

    에러 수정 완료

--- src/main/java/psnl/liar/controller/ChatController.java
+++ src/main/java/psnl/liar/controller/ChatController.java
@@ -3,7 +3,6 @@ package psnl.liar.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
